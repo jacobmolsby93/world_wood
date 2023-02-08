560c7a3e8589d7eb3b5c3d35959bf31fe4e35dc4
jacobmolsby93 <jacobmolsby@hotmail.se>
2023-02-08 13:54:05 +0100

    Add List view,urls

--- backend/wood/urls.py
+++ backend/wood/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import WoodList
 
 urlpatterns = [
-    
+    path('wood/', WoodList.as_view(), name="wood-list")
 ]
--- backend/wood/views.py
+++ backend/wood/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
+from .serializers import WoodSerializer
+from .models import Wood
 
 # Create your views here.
+
+
+# List View
+class WoodList(generics.ListAPIView):
+    queryset = Wood.objects.all()
+    serializer_class = WoodSerializer
+
+
