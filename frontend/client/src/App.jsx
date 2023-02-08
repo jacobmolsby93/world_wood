6113223d6a3f2e7984c3187793e470635a59815c
jacobmolsby93 <jacobmolsby@hotmail.se>
2023-02-08 13:22:35 +0100

    Initialize frontend

---
+++ frontend/client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import {Home} from './pages/home/index.jsx'
+
+function App() {
+
+  return (
+    <div className="app">
+      <Home />
+    </div>
+  )
+}
+
+export default App
